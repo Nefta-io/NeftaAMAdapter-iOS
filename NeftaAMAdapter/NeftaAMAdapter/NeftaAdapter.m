36bf670e8a8c4e990106f8c1af300e4e945d29ed
Tomaz Treven <treven@nefta.io>
2024-12-20 10:25:12 +0100

    Updated privacy manifest Added method to provide tracking consent status Minor fixes

--- NeftaAMAdapter/NeftaAMAdapter/NeftaAdapter.m
+++ NeftaAMAdapter/NeftaAMAdapter/NeftaAdapter.m
@@ -25,7 +25,7 @@ static NeftaPlugin *_plugin;
 }
 
 + (GADVersionNumber)adapterVersion {
-    GADVersionNumber version = {2, 0, 0};
+    GADVersionNumber version = {2, 0, 1};
     return version;
 }
 
