3186c4f52dd5de71f4b38ee9c752c5d2fe555cc4
Tomaz Treven <treven@nefta.io>
2026-05-18 11:13:28 +0200

    Improved threaded impression handling, minimal deployment target is now explicit

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "NeftaSDK",
             url: "https://github.com/Nefta-io/NeftaSDK-iOS/releases/download/REL_4.5.3/NeftaSDK.xcframework-4.5.3.zip",
-            checksum: "2c035a98a1f5f4b02cd8afb7e5e873dfd7bad2e6077233e4e615af551bd943ed"
+            checksum: "81279a1f3992f0b4e45f6c3fa45609863e92c88cd96d6a293c9b5a41cf3ba28b"
         )
     ]
 )
